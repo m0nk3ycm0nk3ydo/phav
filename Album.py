10d7a3a406f3ac36989f2eb8d713022aa7f40c46
m0nk3ycm0nk3ydo <53500406+m0nk3ycm0nk3ydo@users.noreply.github.com>
2019-08-11 17:25:53 -0700

    Update Album.py

--- Album.py
+++ Album.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from PIL import Image
 import glob
 import tkinter
@@ -16,6 +15,7 @@ class AlbumSelector:
         self.tgt = tgt
         self.files = glob.glob(self.src)
         self.curIdx = 0
+        self.curImg = None
         self.window = tkinter.Tk()
         self.window.bind("<Key>", self.key_press)
         self.display()
@@ -52,18 +52,50 @@ class AlbumSelector:
         copyfile(s, t)
 
     def display(self):
+
         f = self.files[self.curIdx]
-        print(f)
         self.window.title(f)
+        self.clear()
+
         photo = Image.open(f)
+        print("screen {},{}".format(self.window.winfo_screenwidth(),  self.window.winfo_screenheight()))
+        print("photo  {},{}".format(photo.size[0], photo.size[1]))
+
+        photo = self.resize_to_screen(photo)
         tk_photo = ImageTk.PhotoImage(photo)
-        self.window.geometry("{}x{}+100+100".format(photo.size[0], photo.size[1]))
-        image_widget = tkinter.Label(self.window, image=tk_photo)
-        image_widget.place(x=0, y=0, width=photo.size[0], height=photo.size[1])
+        self.window.geometry("%dx%d+0+0" % (self.window.winfo_screenwidth(),  self.window.winfo_screenheight()))
+        self.curImg = tkinter.Label(self.window, image=tk_photo)
+        self.curImg.place(x=0, y=0, width=photo.size[0], height=photo.size[1])
         self.wait_for_input()
+
+    def clear(self):
+        if self.curImg:
+            self.curImg.pack_forget()
+            self.curImg.destroy()
+
+    def resize_to_screen(self, photo):
+        screen_width, screen_height = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        photo_width, photo_height = photo.size[0], photo.size[1]
+
+        if photo_width >= photo_height:
+            if photo_width <= screen_width and photo_height <= screen_height:
+                return photo
+            wr = screen_width / photo_width
+            hr = screen_height / photo_height
+        else:
+            if photo_height <= screen_height and photo_width <= screen_width:
+                return photo
+            wr = screen_height / photo_width
+            hr = screen_width / photo_height
+
+        resize_ratio = min(wr, hr)
+        new_width = photo_width * resize_ratio
+        new_height = photo_height * resize_ratio
+
+        return photo.resize((int(new_width), int(new_height)))
 
     def wait_for_input(self):
         self.window.mainloop()
 
 
-sel = AlbumSelector("/users/andreigoldfain/Downloads/*.jpg", "/users/andreigoldfain/Downloads/phav")
+sel = AlbumSelector("/Users/andreigoldfain/Documents/img-test/*.JPG", "/users/andreigoldfain/Documents/imgsave")
